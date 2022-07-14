b96111862526948f4efa3b7c651847a571a458b7
Kyle Thomas <kylejwthomas@gmail.com>
2022-07-14 17:44:27 +0900

    react intro

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>Apple</li>
+        <li>Banana</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
