369a5e99c8e5bfa678736de944ca8bf1bb18178f
Kyle Thomas <kylejwthomas@gmail.com>
2022-07-16 19:02:15 +0900

    organizing components

--- src/index.js
+++ src/index.js
@@ -1,43 +1,17 @@
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-
-
-//   </React.StrictMode>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
 import React from "react"
 import ReactDOM from "react-dom"
+import Header from "./Header"
+import Footer from "./Footer"
+import MainContent from "./MainContent"
 
-function Page() {
+function App() {
     return (
-      <div>
-        <header>
-            <nav>
-                <img src="./logo192.png" width="40px" alt="site" />
-            </nav>
-        </header>
-        <h1>Reasons I'm excited to learn React</h1>
-        <ol>
-          <li>React is awesome</li>
-          <li>We need it for a job</li>
-          <li>I want to be a better developer</li>
-        </ol>
-        <footer>
-          <small>© 2022 Thomas development. All rights reserved.</small>
-        </footer>
-      </div>
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
     )
 }
 
-ReactDOM.render(<Page />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
